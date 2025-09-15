1a8c48d8831b4a7821c78f0fa07b9523a0ffebe0
r1semir <emirkanulker08@gmail.com>
2025-09-16 02:38:37 +0300

    74

---
+++ classOrnekleri1/src/classOrnekleri1/testClass.java
@@ -0,0 +1,9 @@
+package classOrnekleri1;
+
+public class testClass {
+
+	public int yas = 24;
+	public String isim = "Emirkan";
+	public String soyİsim="Ülker";
+	
+}
---
+++ classOrnekleri1/src/ornek01/classlarArasi.java
@@ -0,0 +1,21 @@
+
+package ornek01;
+import classOrnekleri1.testClass;
+public class classlarArasi {
+
+	public static void main(String[] args) {
+		
+		testClass ben = new testClass();
+		
+		String ismim = ben.isim;
+		String soyIsmim = ben.soyİsim;
+		Integer yasim = ben.yas;
+		
+		System.out.println(ismim);
+		System.out.println(soyIsmim);
+		System.out.println(yasim);
+		
+		
+	}
+	
+}
